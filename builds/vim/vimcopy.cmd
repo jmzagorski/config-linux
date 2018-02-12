6a4a219e57a84aedec1c6ceba6882d3e6aa0072b
Jeremy Zagorski <jmzagorski@gmail.com>
2018-02-12 08:53:47 -0500

    refactor(vim): put vim exe in proper wins location

--- builds/vim/vimcopy.cmd
+++ builds/vim/vimcopy.cmd
@@ -1,7 +1,7 @@
 :: Git
 :: clone https://github.com/vim/vim
 @set SRC=%USERPROFILE%\builds\vim\src
-@set DST=C:\bin\vim\vim80
+@set DST=%ProgramFiles(x86)%\vim\vim80
 xcopy %SRC%\runtime "%DST%" /D /E /H /I /Y %*
 xcopy %SRC%\src\xxd\xxd.exe "%DST%\*" /D /Y %*
 xcopy %SRC%\src\GvimExt\gvimext.dll "%DST%\*" /D /Y %*
